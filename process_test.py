0e5db40614776e7a5d5687da661e9ecb00cc53c9
alex <919460849@qq.com>
2019-04-30 14:43:12 +0800

    1

---
+++ process_test.py
@@ -0,0 +1,20 @@
+from multiprocessing import Pool
+
+import time
+
+import os
+
+def work(num):
+    print("hello word")
+    for i in range(5):
+        print("---pid=%s--%d---%d--"%(os.getpid(),i,num))
+    time.sleep(2)
+
+po = Pool(3)
+
+for i in range(10):
+    po.apply_async(work,(i,))
+
+po.close()
+
+po.join()
---
+++ python_test.py
@@ -0,0 +1,208 @@
+# print("dasda\tasdas")
+#
+# print("dasda\nasdas")
+#
+# print("dasda\sdas")
+#
+# print("'isn't',she said")
+#
+# list = [1,2,3,4,5,6,7]
+#
+# res = list[2:4]
+#
+# list[1] = 666
+#
+# # print(list)
+#
+# print(list[:])
+#
+# list.append(88)
+#
+# print(list)
+#
+# result = len(list)
+#
+# print(result)
+#
+# n = ["a","b","c"]
+#
+# x = ["body",list,n]
+#
+# print(x)
+#
+# print(x[1][1])
+#
+# x.pop(0)
+#
+# print(x)
+#
+# x.pop(1)
+#
+# print(x)
+
+
+# tup1 = (1,2,3,4,5,)
+#
+# print(tup1)
+#
+# res = tup1[0:3]
+#
+# print(res)
+#
+# tup2 = ("a","b","c","d")
+
+# print(tup2)
+#
+# tup_new = tup1 + tup2
+#
+# print(tup_new)
+#
+# print(tup_new[3])
+#
+# print(max(tup1))
+#
+# print(tup1[-2])
+#
+# print(tup1[1:])
+#
+# print(tup1[2])
+
+
+# dict = {"a":"123","b":"456","c":"789"}
+#
+# print(dict)
+
+# res = dict.clear()
+#
+# print(res)
+
+# res = "a" in dict
+#
+# print(res)
+
+# result = dict.items()
+#
+# print(result)
+
+# res = dict.values()
+#
+# print(res)
+
+# res = dict.get("a")
+#
+# print(res)
+
+
+# get_num = int(input("请输入："))
+#
+# if 70<get_num <=100:
+#     print("num_1")
+#
+# elif 70<=get_num<=90:
+#     print("num_2")
+#
+# else:
+#     print("num_3")
+
+# sum = 0
+#
+# for i in range(101):
+#     sum  +=i
+# print(sum)
+#
+#
+# list = [1,2,3,4,5,6,7,8,9]
+#
+# for i in list:
+#     print(i)
+#     if i == 6:
+#         print("over")
+#         break
+
+
+# for i in range(0,12,2):
+#     print(i)
+
+# list = [1,2,3,4,5,6,7,8]
+#
+# print(list)
+#
+# it = iter(list)
+
+# print(next(it))
+# print(next(it))
+# print(next(it))
+
+# for i in it:
+#     print(i,end=" ")
+
+
+# list = [1,2,3,4,2,2]
+
+# list.insert(2,666)
+#
+# print(list)
+#
+# list.remove(4)
+#
+# print(list)
+
+# res = list.pop(0)
+#
+# print(res)
+#
+# print(list)
+
+# print(list.index(3))
+#
+# print(list.count(2))
+#
+# list.sort()
+#
+# print(list)
+
+# list.reverse()
+#
+# print(list)
+
+
+# list = [3,4,5]
+#
+# list.append(6)
+#
+# print(list)
+#
+# list.append(7)
+#
+# print(list)
+#
+#
+# print(list.pop())
+#
+# print(list.pop())
+# print(list.pop())
+# print(list.pop())
+# print(list.pop())
+
+
+# list = [2,3,4]
+#
+# print([i * 2 for i in list])
+#
+# print([[i * 2,i ** 3] for i in list])
+#
+# list = ["a","  b","c    "]
+#
+# print([i.strip() for i in list])
+
+
+str_res = 'gjkkahhikmakkll'
+
+str_result = str_res.split("a")
+
+res = ''.join(str_result)
+
+print(res)
+
+
+
