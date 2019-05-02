c7e3323b742d28291e5ca4581202dc004ec4d20c
alex <919460849@qq.com>
2019-05-02 19:26:23 +0800

    "4"

---
+++ threading_test.py
@@ -0,0 +1,19 @@
+from threading import Thread
+import time
+
+
+def test(id):
+    print("hello word %s !"%id)
+    time.sleep(1)
+
+
+def main():
+    for i in range(10):
+        p = Thread(target=test,args=(i,))
+        p.start()
+
+    # for i in range(10):
+    #     test()
+
+if __name__ == '__main__':
+    main()
