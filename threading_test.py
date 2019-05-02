80f74536ac9a68e80803c2f82698a5184e221a22
alex <919460849@qq.com>
2019-05-02 19:31:22 +0800

    "2"

--- threading_test.py
+++ threading_test.py
@@ -5,6 +5,7 @@ import time
 def test(id):
     print("hello word %s !"%id)
     time.sleep(1)
+    print("123")
 
 
 def main():
@@ -14,6 +15,8 @@ def main():
 
     # for i in range(10):
     #     test()
+
+
 
 if __name__ == '__main__':
     main()
